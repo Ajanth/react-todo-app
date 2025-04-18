e5c48da755626b446d9ddc8d44c210f2d55bc1c9
Ajanth <ajanth2newearth@gmail.com>
2025-04-18 22:40:13 +0200

    feat: setup theme and remove unused styles

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#673ab7", // Purple color
+    },
+  },
+});
+
+export default theme;
