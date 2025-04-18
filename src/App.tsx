6269ccbb19dc2f25b15b20c125f71e79207f86e8
Ajanth <ajanth2newearth@gmail.com>
2025-04-18 23:15:03 +0200

    fix: update header text in App component for testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <MainLayout>
       <div>
-        <h1>Todo App</h1>
+        <h1>Hi, this is a test header, remove it when you start working</h1>
       </div>
     </MainLayout>
   );
